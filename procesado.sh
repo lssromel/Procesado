5895c776fe9eda96c3cc05364fc91f2dd2e151d4
lssromel <lssromel@hotmail.com>
2017-06-14 01:59:40 -0500

    Add files via upload

---
+++ procesado.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo docker create --name tmp -it procesado
+sudo docker start tmp
+sudo docker cp tmp_procesado/prueba.py tmp:/workspace/Procesado/prueba2.py
+sudo docker exec tmp python /workspace/Procesado/prueba2.py
+sudo docker cp tmp:/workspace/Procesado/prueba.html tmp_resultados/prueba.html
+sudo docker stop tmp
+sudo docker rm tmp
