8063bece62fc45223048483a88bbc89fdd2af023
lssromel <lssromel@hotmail.com>
2017-06-14 01:56:53 -0500

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 from bokeh.models.widgets import Panel, Tabs
-from bokeh.io import output_file, show
+from bokeh.io import output_file, show,save
 from bokeh.plotting import figure
 from bokeh.charts import Bar, output_file, show
 from bokeh.embed import file_html
@@ -7,6 +7,7 @@ from bokeh.resources import CDN
 import pandas as pd
 from pymongo import MongoClient
 import ConfigParser
+import pymongo
 
 Config = ConfigParser.ConfigParser()
 Config.read("/workspace/Procesado/mongo.ini")
@@ -19,30 +20,26 @@ user=client["renting"]
 archivos_cargados=user["Archivos_Cargados"]
 tabla=archivos_cargados["viajes"]
 
-db_time = user["time"]
-tabla_time = db_time["viajes"]
-cursor=time.find().limit(1).sort([('time', pymongo.DESCENDING)])
-time=list(cursor)[0]["time"]
-
 cursor = tabla.find()
 df = pd.DataFrame(list(cursor))
+output_file("/workspace/Procesado/prueba.html")
 
-output_file("prueba.html")
 
-p = Bar(df.iloc[0:50], label='PLACA', values="Rendimiento menta combustible Meta (Km/Galon)"+str(list(cursor)[0]["time"]),agg="mean",color="PESO_BRUTO_VEH KG", plot_width=1500, plot_height=1000,
-        title="Desviaciones en los precios de insumos_actividad_unidad",legend=None)
-p.legend.location = "top_left"
-p.legend.click_policy="hide"
+db_time = user["time"]
+tabla_time = db_time["viajes"]
+cursor=tabla_time.find().limit(1).sort([('time', pymongo.DESCENDING)])
+tiempo=str(list(cursor)[0]["time"])
+
+p = Bar(df.iloc[0:50], label='PLACA', values="Rendimiento menta combustible Meta (Km/Galon)",agg="mean",color="PESO_BRUTO_VEH KG", plot_width=1500, plot_height=1000,
+        title="Desviaciones en los precios de insumos_actividad_unidad"+tiempo)
 tab1 = Panel(child=p, title="50")
 
-p2 = Bar(df.iloc[0:20], label='PLACA', values="Rendimiento menta combustible Meta (Km/Galon)"+str(list(cursor)[0]["time"]),agg="mean",color="PESO_BRUTO_VEH KG", plot_width=1500, plot_height=1000,
-        title="Desviaciones en los precios de insumos_actividad_unidad",legend=None)
-p2.legend.location = "top_left"
-p2.legend.click_policy="hide"
+p2 = Bar(df.iloc[0:20], label='PLACA', values="Rendimiento menta combustible Meta (Km/Galon)",agg="mean",color="PESO_BRUTO_VEH KG", plot_width=1500, plot_height=1000,
+        title="Desviaciones en los precios de insumos_actividad_unidad"+tiempo)
 tab2 = Panel(child=p2, title="20")
 
 tabs = Tabs(tabs=[ tab1, tab2 ])
 
-html = file_html(tabs, CDN, "Prueba")
-
+#html = file_html(tabs, CDN, "Prueba")
+save(tabs)
 client.close()
